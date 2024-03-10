17f7e162ddbc7c342cc392afa817c61f32c2507d
Alison Marques <alisonclayton.aac@gmail.com>
2024-03-10 01:15:04 -0300

    Adding update to the menu of the extract-file-names.bat batch script, creating the following three output options: 1 output to a .txt text file, 2 output to the prompt view, 3 both at the same time.

--- extract-file-names.bat
+++ extract-file-names.bat
@@ -13,13 +13,51 @@ echo. Please, enter the absolute directory path to extract name files!
 echo =====================================================================
 echo.
 set /p opdir=Directory:
-echo.
 cd %opdir%
 
-dir /b > %homedir%\names-docs.txt
+cls
+echo.
+echo =====================================================================
+echo. What output type do you prefere?
+echo. 
+echo. 1 The output in a .txt text file.
+echo. 2 The output in the console.
+echo. 3 Both output types.
+echo =====================================================================
+echo.
+set /p opoutput=Output type:
+echo.
+if %opoutput%==1 goto 1
+if %opoutput%==2 goto 2
+if %opoutput%==3 goto 3
 
+:1
+dir /b > %homedir%\names-docs.txt
+cls
 echo.
 echo Your output is in a file named names-docs.txt located in this directory.
+echo.
+goto end
+
+:2
+cls
+echo.
+echo Your output:
+echo.
+dir /b
+goto end
+
+:3
+dir /b > %homedir%\names-docs.txt
+cls
+echo.
+echo This output is in a file named names-docs.txt located in this directory.
+echo.
+dir /b
+echo.
+goto end
+
+:end
 echo.
 echo Press any key to end. . .
 pause>nul
