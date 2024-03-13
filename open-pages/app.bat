cf77463fc03fe889470e3555e7fa14e9dd0c060a
Alison Marques <alisonclayton.aac@gmail.com>
2024-03-13 16:34:40 -0300

    Started the creation of FAVORITE PAGES LAUNCHER batch script app, with the creation of the main functions: launch, startmenu, openURL, saveURL, invalideop and forward functions.

---
+++ open-pages/app.bat
@@ -0,0 +1,100 @@
+@echo off
+color 0a
+Title FAVORITE PAGES LAUNCHER
+cls
+
+:launch
+cls
+echo. 
+echo. :: FAVORITE PAGES LAUNCHER ::
+echo. - press space+enter to exit -
+echo.
+set /p dolaunch="Do you want to launch favorites and exit? [y/n]: "
+echo.
+if %dolaunch%==y (
+    set globalfw=exit
+    goto openURL)
+if %dolaunch%==n goto startmenu 
+
+set globalbk=launch
+goto invalideop
+
+:startmenu
+cls
+echo. 
+echo. :: FAVORITE PAGES LAUNCHER ::
+echo. - press space+enter to exit -
+echo.
+echo =====================================================================
+echo. Please, chose an opition:
+echo.
+echo. 1 Save new URL
+echo. 2 List URL
+echo. 3 Delete URL
+echo. 4 Open URLs
+echo =====================================================================
+echo.
+set /p opmenu=Option:
+if not defined opmenu goto startmenu
+if %opmenu%==1 goto saveURL
+if %opmenu%==2 goto listURL
+if %opmenu%==3 goto deleteURL
+if %opmenu%==4 (
+    set globalfw=startmenu
+    goto openURL)
+
+set globalbk=startmenu
+goto invalideop
+
+:openURL
+cls
+for /f "delims=" %%i in ('type urls.txt') do (
+    start "" "%%i"
+    echo Opening "%%i")
+echo.
+echo All URLs was opened!
+
+if %globalfw%==exit exit
+
+goto forward
+
+:saveURL
+cls
+echo. 
+echo.       :: SAVING URL ::
+echo. - press space+enter to exit -
+echo. 
+echo =====================================================================
+echo. Please, enter the URL of a new favorite page to save. . .
+echo =====================================================================
+echo.
+set /p url=URL:
+if not defined url goto saveURL
+echo %url% >> urls.txt
+echo URL save successfully!
+echo.
+
+set /p savenew="Do you want to save new URL? [y/n]: "
+if %savenew%==y goto saveURL
+if %savenew%==n goto startmenu 
+
+set globalbk=saveURL
+goto invalideop
+
+:listURL
+
+:deleteURL
+
+rem exit
+
+:invalideop
+echo. 
+echo Invalid option, press any key to try again. . .
+pause>nul
+goto %globalbk%
+
+:forward
+echo. 
+echo Press any key to continue. . .
+pause>nul
+goto %globalfw%
